1f47f73b5f028ba0f2f954de49d9b2a142a1794a
wuhua <memovinda@outlook.com>
2021-09-24 16:38:44 +0800

    更改框架为vue

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
-# Clap
-小游戏，使用cocos creator作为框架. 
+# clap
+> 小游戏，使用vue作为框架. 
 ## Todo
 - [ ] 基本界面
 - [ ] 角色
 - [ ] 技能
 - [ ] 联机
 - [ ] 规则
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run e2e tests
+npm run e2e
+
+# run all tests
+npm test
+```
+
+For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/components/Start.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="interface">
+    <div id="sword" class="column">
+      <div class="row">
+        <button>sword</button>
+      </div>
+      <div class="row">
+        <label>血量</label>
+      </div>
+      <div class="row">
+        <label>初始能量</label>
+      </div>
+            <div class="row">
+        <button>技能详情</button>
+      </div>
+    </div>
+    <div id="mana" class="column">
+      <div class="row">
+        <button>mana</button>
+      </div>
+      <div class="row">
+        <label>血量</label>
+      </div>
+      <div class="row">
+        <label>初始能量</label>
+      </div>
+            <div class="row">
+        <button>技能详情</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Start",
+};
+</script>
+
+<style scoped>
+.column {
+  float: left;
+  width: 50%;
+}
+</style>
